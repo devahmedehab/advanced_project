7cdd4c6844ca90943f0609c6461f4114b4792867
news <89398483+devahmedehab@users.noreply.github.com>
2023-02-27 23:05:10 +0200

    initial

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,6 +1,7 @@
 
 import 'package:app1/data/data_source/remote_data_source.dart';
 import 'package:app1/data/mapper/mapper.dart';
+import 'package:app1/data/network/error_handler.dart';
 import 'package:app1/data/network/failure.dart';
 import 'package:app1/data/network/network_info.dart';
 import 'package:app1/data/network/requests.dart';
@@ -16,22 +17,30 @@ class RepositoryImpl implements Repository{
   Future<Either<Failure, Authentication>> login(
       LoginRequests loginRequests) async {
     if(await _networkInfo.isConnected){
-        //call API
-      final response =await _remoteDataSource.login(loginRequests);
 
-      if (response.status ==0){
-        //success
-        //return either right
-        //return data
-        return Right(response.toDomain());
-      }else{
-        //return either left
-        //failure -- business error
-        return Left(Failure(409, response.message ?? "business error"));
+      try{
+        final response =await _remoteDataSource.login(loginRequests);
+
+        if (response.status ==ApiInternalStatus.SUCCESS){
+          //success
+          //return either right
+          //return data
+          return Right(response.toDomain());
+        }else{
+          //return either left
+          //failure -- business error
+          return Left(Failure(ApiInternalStatus.FAILURE, response.message ?? ResponseMessage.DEFAULT));
+        }
+
+      }catch(error){
+        return Left(ErrorHandler.handle(error).failure);
+
       }
+        //call API
+
 
     }else{
-      return Left(Failure(501,  "please check your internet connection"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
       //return either left
       //return internet connection error
     }
